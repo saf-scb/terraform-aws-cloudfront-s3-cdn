0b8069db862cc64ca05b345a3792e894017c841d
Gabe Levasseur <gabel0287@gmail.com>
2017-12-22 16:39:07 -0800

    Create custom error handling for React and Angular

--- main.tf
+++ main.tf
@@ -192,6 +192,18 @@ resource "aws_cloudfront_distribution" "default" {
     }
   }
 
+  custom_error_response {
+    error_code = 403
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
   tags = "${module.distribution_label.tags}"
 }
 
